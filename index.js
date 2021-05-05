e9c36b2508b6719c26ccdf3ab71182feed353997
NguyenMinhVuong <vuong.nm@briswell-vn.com>
2021-05-05 13:26:12 +0700

    finish

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 4000;
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! successful ~~~~~');
 });
 app.listen(port, function () {
   console.log('Listening on port ' + port);
